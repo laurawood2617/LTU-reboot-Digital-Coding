ca9b8a69e8a7f8a554bacf9f1f89f1d92d728996
laurawood2617 <laurawood2617@gmail.com>
2020-11-24 11:16:14 +0000

    Update task-3.1.4.js
    Added two vars and did basic arithmetic using them

---
+++ Module-3/Scripts/task-3.1.4.js
@@ -0,0 +1,11 @@
+var myage = 23;
+
+var cousinsage = 5;
+
+document.write (myage - cousinsage);
+
+console.log (myage * cousinsage);
+
+document.write (myage + cousinsage);
+
+console.log (myage / cousinsage);
