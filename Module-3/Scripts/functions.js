01185ab51e6fa8eb131e5c5eaffa9ea822899c1e
laurawood2617 <laurawood2617@gmail.com>
2020-11-25 10:08:43 +0000

    Update functions.js
    Add function with multiple parameters

--- Module-3/Scripts/functions.js
+++ Module-3/Scripts/functions.js
@@ -9,12 +9,25 @@ function functionName (parameters) {
     function body 
 
     Code goes here
-    
+
+
 }
 
+Once we have created a function we need to envoke it and ask it to do what it should. We do this by writing the name of the function and the parameters of the function on a separate line of code
+
+You can add more than one parameter to a specific function, these can be called arguments.
 */
 
-function greeting (Laura) {
+function greeting (name) {
 
-    console.log(Laura);
+    console.log('Hello ' + name);
 }
+
+greeting ('Laura');
+
+function nameAndAge (name, age) {
+
+    console.log (name + ' is ' + age + 'years old');
+}
+
+nameAndAge ('Laura', 23);
