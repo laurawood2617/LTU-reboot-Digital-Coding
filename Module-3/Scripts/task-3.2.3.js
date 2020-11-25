46146b0cccb7a6b05edc7c571b9d9bd89ff1b53f
laurawood2617 <laurawood2617@gmail.com>
2020-11-25 10:50:44 +0000

    Upate task-3.2.3.js
    Add return function

---
+++ Module-3/Scripts/task-3.2.3.js
@@ -0,0 +1,9 @@
+function fullname (firstName, lastName) {
+    return( firstName + lastName);
+}
+
+fullname('Laura ', 'Wood');
+
+var personName = fullname {'James ', 'Bond'};
+
+var personName = fullname {'Jack ', 'Smith'}
