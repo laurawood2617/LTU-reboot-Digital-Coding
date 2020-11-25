55ec77861cc7546ed42162f9fc6f678c45dba3b7
laurawood2617 <laurawood2617@gmail.com>
2020-11-25 11:05:35 +0000

    Update scope.js
    Added global and local scope

--- Module-3/Scripts/scope.js
+++ Module-3/Scripts/scope.js
@@ -1,7 +1,22 @@
 //global scope
+//can be accessed anywhere within the code
 
 //local scope
+//only accessible within a function in the code. Can only be made accessible using return function.
 
 var globalScope = 'Global Scope';
 
-console. log (globalScope);
+console.log (globalScope);
+
+function scope() {
+    var localScope = 'Local Scope';
+
+    console.log(globalScope);
+
+    return localScope;
+}
+
+var scopeValue= scope();
+
+console.log (scopeValue);
+
