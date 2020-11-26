158e9f802af335f97055391080ffbf635558ad21
laurawood2617 <laurawood2617@gmail.com>
2020-11-26 11:08:06 +0000

    Update task-3.4.2.js
    Added comment title

--- Module-3/Scripts/task-3.4.2.js
+++ Module-3/Scripts/task-3.4.2.js
@@ -1,3 +1,4 @@
+/* Creating an array and using for variable to loop over array and create list */
 var favouriteFoods =['Chocolate', 'Burgers', 'Cheese', 'Pigs in Blankets', 'Houmous', 'Olives', 'Marmite', 'Parsnips', 'Apples', 'Bananas', 'Strawberries', 'Ice-cream'];
 
 for(var i=0; i < favouriteFoods.length; i++) { 
