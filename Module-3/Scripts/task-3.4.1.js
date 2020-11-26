a4e5ed5352177c7204349d71ff4e4ac09a205b21
laurawood2617 <laurawood2617@gmail.com>
2020-11-26 10:16:08 +0000

    Update task-3.4.1.js
    Added for loop and applied variables for 7x table

---
+++ Module-3/Scripts/task-3.4.1.js
@@ -0,0 +1,13 @@
+for  (var i=1; i<=12; i++) {
+    var number = i*7;
+
+    console.log (number);
+}
+
+for (var i=1; i<=12; i++) {
+    var number = i*7;
+
+    var result = i + number;
+
+    console.log ('7' + '*' + i + '=' +result);
+}
