6d16bf5d97afd0c7641e8a69ee647455c8a6e4f0
laurawood2617 <laurawood2617@gmail.com>
2020-11-26 10:57:49 +0000

    Update task-3.4.3.js
    Add array and loop over on document write

---
+++ Module-3/Scripts/task-3.4.3.js
@@ -0,0 +1,5 @@
+var favouriteFoods =['Chocolate', ' Burgers', ' Cheese', ' Pigs in Blankets', ' Houmous', ' Olives', ' Marmite', ' Parsnips', ' Apples', ' Bananas', ' Strawberries', ' Ice-cream'];
+
+for(var i=0; i < favouriteFoods.length; i++) { 
+
+document.write (favouriteFoods [i]);}
