6a47b8de34115266556f00b885daeab17ac8e8e5
laurawood2617 <laurawood2617@gmail.com>
2020-11-26 11:07:03 +0000

    Update task-3.4.3.js
    Add comment title

--- Module-3/Scripts/task-3.4.3.js
+++ Module-3/Scripts/task-3.4.3.js
@@ -1,3 +1,4 @@
+/* Creating an array and presenting in doc write */
 var favouriteFoods =['Chocolate', ' Burgers', ' Cheese', ' Pigs in Blankets', ' Houmous', ' Olives', ' Marmite', ' Parsnips', ' Apples', ' Bananas', ' Strawberries', ' Ice-cream'];
 
 for(var i=0; i < favouriteFoods.length; i++) { 
