ca521158ba65a8235930680959b1376a5162990e
laurawood2617 <laurawood2617@gmail.com>
2020-11-26 10:39:45 +0000

    Update task-3.4.2.js
    Created array and for loop over

---
+++ Module-3/Scripts/task-3.4.2.js
@@ -0,0 +1,7 @@
+var favouriteFoods =['Chocolate', 'Burgers', 'Cheese', 'Pigs in Blankets', 'Houmous', 'Olives', 'Marmite', 'Parsnips', 'Apples', 'Bananas', 'Strawberries', 'Ice-cream'];
+
+for(var i=0; i < favouriteFoods.length; i++) { 
+
+console.log (favouriteFoods [i]);
+
+}
